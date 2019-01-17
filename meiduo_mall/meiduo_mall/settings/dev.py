a367ead4b104cea58cddba02e71ea29e309f41d6
xwp <xwp_fullstack@163.com>
2019-01-17 15:36:33 +0800

    02.项目配置　注册DRF、数据库配置、日志信息处理

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 # 开发模式
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# 简化导包路径
+# 添加要导包的路径到 path 中，添加列表的第一位
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+
 
 # Application definition
 
@@ -37,7 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'meiduo_mall.apps.users',
+    # 注册第三方应用　
+    'rest_framework',   # drf框架(应用)
+
+
+    # 注册自己的应用
+    # 'meiduo_mall.apps.users',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -80,8 +91,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST':'localhost',
         'PORT':'3306',
-        'USER':'root',
-        'PASSWORD':'mysql',
+        'USER':'tb_meiduo',
+        'PASSWORD':'meiduo',
         'NAME': 'tb_meiduo',
     }
 }
@@ -109,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
+# 语言和时区
 LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
@@ -124,3 +136,74 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# 1. 配置redis数据库    2. 把session信息存储在redis中
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+# 保存 session数据到 Redis中
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
+
+# 配置日志文件
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
+    'formatters': {  					# 日志信息显示的格式
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_true': {  	# django在debug模式下才输出日志
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {  		# 日志处理方法
+        'console': {  	# 向终端中输出日志
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': { 		 # 向文件中输出日志
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            # 日志文件的位置
+            'filename': os.path.join(os.path.dirname(BASE_DIR), "logs/meiduo.log"),
+            'maxBytes': 300 * 1024 * 1024,   # 日志文件的最大容量
+            'backupCount': 10,
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {  	# 定义了一个名为django的日志器
+            'handlers': ['console', 'file'],  	# 可以同时向终端与文件中输出日志
+            'level': 'INFO',  					# 日志器接收的最低日志级别
+            'propagate': True
+        },
+    }
+}
+
+# DRF相关配置
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'meiduo_mall.utils.exceptions.custom_exception_handler',
+}
+
